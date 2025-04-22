34a1244a53a01c47c53a31848d7f98128d11ca40
hyemin <hmpark15@gmail.com>
2025-04-22 19:38:51 +0900

    feat: 연락처 추가 화면 UI 구현

--- ContactApp/AddContactViewController.swift
+++ ContactApp/AddContactViewController.swift
@@ -6,10 +6,82 @@ import SnapKit
 
 class AddContactViewController: UIViewController {
     
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.borderColor = UIColor.gray.cgColor
+        imageView.layer.borderWidth = 2
+        imageView.layer.cornerRadius = 60
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "profile")
+        return imageView
+    }()
+    
+    private let setRandomImageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("랜덤 이미지 생성", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        return button
+    }()
+    
+    private let nameTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 5
+        return textField
+    }()
+    
+    private let phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 5
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "연락처 추가"
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "적용", style: .plain, target: nil, action: nil)
+        configureUI()
+        
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+        
+        [
+            profileImageView,
+            setRandomImageButton,
+            nameTextField,
+            phoneNumberTextField
+        ].forEach { view.addSubview($0) }
+        
+        profileImageView.snp.makeConstraints {
+            $0.width.height.equalTo(120)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+        }
+        
+        setRandomImageButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
+        }
+        
+        nameTextField.snp.makeConstraints {
+            $0.top.equalTo(setRandomImageButton.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.height.equalTo(30)
+        }
+        
+        phoneNumberTextField.snp.makeConstraints {
+            $0.top.equalTo(nameTextField.snp.bottom).offset(10)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.height.equalTo(30)
+        }
         
     }
     
